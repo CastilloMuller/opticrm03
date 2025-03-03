f9935a59c54e6919ced41fd268d98c1e32252bf3
CastilloMuller <info@levere.nl>
2025-03-03 14:17:27 +0100

    Create communication-history.js

---
+++ pages/communication-history.js
@@ -0,0 +1,9 @@
+export default function CommunicationHistory() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Communication History</h1>
+      <p>This page will show a timeline of communication entries such as calls, emails, and notes.</p>
+      {/* Future: Add historical entry features and editing capabilities */}
+    </div>
+  );
+}
