c0451100c344550bddda58b5c0f72218c84882e6
CastilloMuller <info@levere.nl>
2025-03-03 14:16:37 +0100

    Create tasks.js

---
+++ pages/tasks.js
@@ -0,0 +1,9 @@
+export default function Tasks() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Task Management</h1>
+      <p>This page will display and manage tasks.</p>
+      {/* Future: Implement task creation, history, and completion */}
+    </div>
+  );
+}
