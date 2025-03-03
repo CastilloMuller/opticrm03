85929f399e49c5aed1fba11d0b20a81d265383da
CastilloMuller <info@levere.nl>
2025-03-03 14:18:23 +0100

    Create data-management.js

---
+++ pages/data-management.js
@@ -0,0 +1,9 @@
+export default function DataManagement() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Data Management</h1>
+      <p>This page will allow searching, filtering, and CSV import/export for leads.</p>
+      {/* Future: Add search, filter, sort, and CSV import/export functionality */}
+    </div>
+  );
+}
