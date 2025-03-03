ceeff3c0b9637416acc0ddcd9e80cb512fed6146
CastilloMuller <info@levere.nl>
2025-03-03 14:17:02 +0100

    Create calendar.js

---
+++ pages/calendar.js
@@ -0,0 +1,9 @@
+export default function Calendar() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Calendar Integration</h1>
+      <p>This page will display calendar events and tasks.</p>
+      {/* Future: Implement interactive calendar with color coding */}
+    </div>
+  );
+}
