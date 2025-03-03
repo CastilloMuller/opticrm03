fedf32fea7ca1db91bc7377ba25002b744267baa
CastilloMuller <info@levere.nl>
2025-03-03 14:17:49 +0100

    Create analytics.js

---
+++ pages/analytics.js
@@ -0,0 +1,9 @@
+export default function Analytics() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Analytics</h1>
+      <p>This page will display various analytics for leads and tasks.</p>
+      {/* Future: Include charts and metrics for pipeline, lead scoring, and performance */}
+    </div>
+  );
+}
