cd9f90acd4d5fb1a1d8ba6e1495821fca5e6228e
CastilloMuller <info@levere.nl>
2025-03-03 14:16:12 +0100

    Create leads.js

---
+++ pages/leads.js
@@ -0,0 +1,9 @@
+export default function Leads() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Lead Management</h1>
+      <p>This page will display and manage leads.</p>
+      {/* Future: Add lead card display, detailed views, and search/filter functionality */}
+    </div>
+  );
+}
